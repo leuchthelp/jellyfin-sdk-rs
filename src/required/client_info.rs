379e93ac0c42ec74d8d07363f00df72e0ac7a00b
leuchthelp <gfpinthehouse@gmail.com>
2026-07-21 20:12:03 +0200

    fix: make info types comparable

--- src/required/client_info.rs
+++ src/required/client_info.rs
@@ -1,3 +1,4 @@
+#[derive(Eq, PartialEq)]
 pub struct ClientInfo {
     pub name: String,
     pub version: String,
--- src/required/device_info.rs
+++ src/required/device_info.rs
@@ -1,3 +1,4 @@
+#[derive(Eq, PartialEq)]
 #[derive(Debug, Clone)]
 pub struct DeviceInfo {
     pub id: String,
